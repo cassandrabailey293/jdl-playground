96365791762fcf4a7714b77a1c01741e4106e124
Cassandra Bailey <cassandra.bailey@connexta.com>
2021-02-12 11:05:33 -0700

    Initial version of jdlplayground generated by JHipster-6.10.5

---
+++ src/main/java/org/jhipster/jdlplayground/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.jhipster.jdlplayground.repository;
+
+import org.jhipster.jdlplayground.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
